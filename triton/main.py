607b1d8b3cbabae9efc5bc1a837ded544015bb18
ThinamXx <thinamjr1@gmail.com>
2025-01-05 09:02:13 +0545

    ☑️  continued the work on implementing the triton kernel

--- triton/main.py
+++ triton/main.py
@@ -3,6 +3,70 @@ import triton
 import triton.language as tl
 
 
+class TritonAttention(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, Q, K, V, causal, softmax_scale):
+        head_dim_Q, head_dim_K, head_dim_V = Q.shape[-1], K.shape[-1], V.shape[-1]
+        assert head_dim_Q == head_dim_K and head_dim_K == head_dim_V
+
+        batch_size, num_heads, seq_len, head_dim = Q.shape
+
+        output = torch.empty_like(Q)
+        stage = 3 if causal else 1
+
+        grid = lambda meta: (
+            triton.cdiv(
+                seq_len, meta["BLOCK_SIZE"]
+            ),  # which block of queries we are working with.
+            batch_size * num_heads,  # which heads of which batch we are working with.
+            1,  # Z dimension.
+        )
+
+        # used to save logsumexp for the backward pass.
+        M = torch.empty(
+            (batch_size, num_heads, seq_len), device=output.device, dtype=torch.float32
+        )
+
+        _attn_fwd[grid](
+            Q=Q,
+            K=K,
+            V=V,
+            softmax_scale=softmax_scale,
+            output=output,
+            M=M,
+            stride_Q_batch=Q.stride(0),
+            stride_Q_head=Q.stride(1),
+            stride_Q_seq=Q.stride(2),
+            stride_Q_dim=Q.stride(3),
+            stride_K_batch=K.stride(0),
+            stride_K_head=K.stride(1),
+            stride_K_seq=K.stride(2),
+            stride_K_dim=K.stride(3),
+            stride_V_batch=V.stride(0),
+            stride_V_head=V.stride(1),
+            stride_V_seq=V.stride(2),
+            stride_V_dim=V.stride(3),
+            stride_output_batch=output.stride(0),
+            stride_output_head=output.stride(1),
+            stride_output_seq=output.stride(2),
+            stride_output_dim=output.stride(3),
+            batch_size=batch_size,
+            num_heads=num_heads,
+            seq_len=seq_len,
+            head_dim=head_dim,
+            stage=stage,
+        )
+
+        ctx.save_for_backward(Q, K, V, output, M)
+        ctx.grid = grid
+        ctx.head_dim = head_dim
+        ctx.causal = causal
+        ctx.softmax_scale = softmax_scale
+
+        return output
+
+
 def test_ops(batch_size, num_heads, seq_len, head_dim, causal, dtype=torch.float16):
     Q = (
         torch.empty(
