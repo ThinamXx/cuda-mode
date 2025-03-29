089d65b6566d096216855fa33312cc5904adf63c
ThinamXx <thinamjr1@gmail.com>
2025-03-29 21:18:39 +0545

    🍵 control divergence

--- cuda/day13/sum_reduction.cu
+++ cuda/day13/sum_reduction.cu
@@ -6,7 +6,7 @@ __global__ void sumReductionKernel(float *input, float *output, int N) {
     unsigned int tid = threadIdx.x;
 
     for (unsigned int stride = 1; stride < N; stride *= 2) {
-        int index = 2 * stride * tid;
+        int index = 2 * stride * tid; // tid is the offset. 
         if (index + stride < N) {
             input[index] += input[index + stride];
         }
@@ -19,13 +19,33 @@ __global__ void sumReductionKernel(float *input, float *output, int N) {
     }
 }
 
-void sumReduction(float *input, float *output, int N) {
+__global__ void sumReductionKernelOptimized(float *input, float *output, int N) {
+    unsigned int tid = threadIdx.x;
+
+    for (unsigned int stride = N / float(2); stride >= 1; stride /= 2) {
+        if (threadIdx.x < stride) {
+            input[tid] += input[tid + stride];
+        }
+
+        __syncthreads();
+    }
+
+    if (threadIdx.x == 0) {
+        *output = input[0];
+    }
+}
+
+void sumReduction(float *input, float *output, float *output_optimized, int N) {
     int size = N * sizeof(float);
 
-    float *d_input, *d_output;
+    float *d_input, *d_input_optimized, *d_output, *d_output_optimized;
 
     // 1. Allocate device memory for the input and output arrays.
     cudaError_t err = cudaMalloc((void**)&d_input, size);
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+    }
+    err = cudaMalloc((void**)&d_input_optimized, size);
     if (err != cudaSuccess) {
         printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
     }
@@ -33,9 +53,14 @@ void sumReduction(float *input, float *output, int N) {
     if (err != cudaSuccess) {
         printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
     }
+    err = cudaMalloc((void**)&d_output_optimized, sizeof(float));
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+    }
 
     // 2. Copy the input array to the device memory.
     cudaMemcpy(d_input, input, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_input_optimized, input, size, cudaMemcpyHostToDevice);
 
     // 3. Launch the kernel. 
     dim3 block(N / 2, 1, 1);
@@ -47,6 +72,7 @@ void sumReduction(float *input, float *output, int N) {
     cudaEventRecord(start);
 
     sumReductionKernel<<<grid, block>>>(d_input, d_output, N);
+    sumReductionKernelOptimized<<<grid, block>>>(d_input_optimized, d_output_optimized, N);
     cudaDeviceSynchronize();
 
     cudaEventRecord(stop);
@@ -57,11 +83,12 @@ void sumReduction(float *input, float *output, int N) {
 
     // 4. Copy the output array to the host memory.
     cudaMemcpy(output, d_output, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(output_optimized, d_output_optimized, sizeof(float), cudaMemcpyDeviceToHost);
 
     // 5. Free the device memory.
     cudaFree(d_input);
     cudaFree(d_output);
-
+    cudaFree(d_output_optimized);
     printf("GPU time taken: %f milliseconds\n", milliseconds);
 }
 
@@ -73,12 +100,14 @@ int main() {
     float input[N] = {4.0, 7.0, 2.0, 3.0, 8.0, 5.0, 9.0, 6.0};
     
     float *output = (float *)malloc(sizeof(float));
+    float *output_optimized = (float *)malloc(sizeof(float));
 
-    sumReduction(input, output, N);
+    sumReduction(input, output, output_optimized, N);
 
     printf("Sum of the input: %f\n", output[0]);
+    printf("Sum of the input optimized: %f\n", output_optimized[0]);
 
     free(output);
-
+    free(output_optimized);
     return 0;
 }
